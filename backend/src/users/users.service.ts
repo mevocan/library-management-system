a6d332811b552d35c855bc3da643cce219728d74
Mevlüt Can Dağ <134000112+mevocan@users.noreply.github.com>
2026-01-08 22:11:42 +0300

    Refactor user creation to handle existing emails

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,29 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
-    constructor(
-        @InjectRepository(User)
-        private usersRepository: Repository<User>,
-    ) {}
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
 
-    async findByEmail(email: string): Promise<User | null> {
-        return this.usersRepository.findOne({ where: { email } });
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const existingUser = await this.usersRepository.findOne({
+      where: { email: createUserDto.email },
+    });
+
+    if (existingUser) {
+      throw new ConflictException('Bu email zaten kayıtlı');
     }
 
-    async findById(id: number): Promise<User | null> {
-        return this.usersRepository.findOne({ where: { id } });
-    }
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
 
-    async create(userData: Partial<User>): Promise<User> {
-        const user = this.usersRepository.create(userData);
-        return this.usersRepository.save(user);
-    }
+    const user = this.usersRepository.create({
+      ...createUserDto,
+      password: hashedPassword,
+    });
 
-    async findAll(): Promise<User[]> {
-        return this.usersRepository.find();
-    }
+    return this.usersRepository.save(user);
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    return this.usersRepository.findOne({ where: { email } });
+  }
+
+  async findById(id: number): Promise<User | null> {
+    return this.usersRepository.findOne({ where: { id } });
+  }
 }
