e3fc6166f785a2442dcc0c6491b4850bc3d82d88
Mevlut Can Dag <mevlutcandag@gmail.com>
2026-01-08 22:19:32 +0300

    Fix: bcryptjs to bcrypt migration and add create-user.dto

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from '../users/users.service';
-import * as bcrypt from 'bcryptjs';
+import * as bcrypt from 'bcrypt';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
 
